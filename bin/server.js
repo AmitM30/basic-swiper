a5e29b563a7233135f0284eb24343ad0b72525f5
AmitM30 <mangalamitash@gmail.com>
2017-03-26 23:55:09 +0530

    code cleanup

--- bin/server.js
+++ bin/server.js
@@ -6,7 +6,7 @@ import {COMPILER_STATS, PORT} from '../webpack.config.base.js';
 
 const debug = _debug('app:server');
 
-config.entry.app.unshift(`webpack-dev-server/client?http://localhost:${PORT}/`, "webpack/hot/dev-server");
+config.entry.app.unshift(`webpack-dev-server/client?http://localhost:${PORT}/`, 'webpack/hot/dev-server');
 const compiler = webpack(config);
 const server = new WebpackDevServer(compiler, {
     hot: true,
